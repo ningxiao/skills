68339353f268b802b82cfd0c0bf1dedbdb68ff84
nxiao <363305175@qq.com>
2026-04-12 01:08:44 +0800

    Add files via upload

---
+++ greeting-skill/scripts/get_greeting.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import argparse
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", type=str, default="张三")
+    args, _ = parser.parse_known_args()
+    print(f"你好，{args.name}！")
